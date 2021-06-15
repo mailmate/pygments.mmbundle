055a49e83e15a8b13888a71daaaf133f0b7ad5fb
Benny Kjær Nielsen <github@freron.com>
2021-06-15 18:16:43 +0200

    Changed: Updated to Pygments 2.9.0.

--- Support/vendor/pygments/lexers/xorg.py
+++ Support/vendor/pygments/lexers/xorg.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 """
     pygments.lexers.xorg
     ~~~~~~~~~~~~~~~~~~~~
 
     Lexers for Xorg configs.
 
-    :copyright: Copyright 2006-2017 by the Pygments team, see AUTHORS.
+    :copyright: Copyright 2006-2021 by the Pygments team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
 
@@ -16,6 +15,7 @@ __all__ = ['XorgLexer']
 
 
 class XorgLexer(RegexLexer):
+    """Lexer for xorg.conf file."""
     name = 'Xorg'
     aliases = ['xorg.conf']
     filenames = ['xorg.conf']
@@ -26,9 +26,9 @@ class XorgLexer(RegexLexer):
             (r'\s+', Text),
             (r'#.*$', Comment),
 
-            (r'((|Sub)Section)(\s+)("\w+")',
-             bygroups(String.Escape, String.Escape, Text, String.Escape)),
-            (r'(End(|Sub)Section)', String.Escape),
+            (r'((?:Sub)?Section)(\s+)("\w+")',
+             bygroups(String.Escape, Text, String.Escape)),
+            (r'(End(?:Sub)?Section)', String.Escape),
 
             (r'(\w+)(\s+)([^\n#]+)',
              bygroups(Name.Builtin, Text, Name.Constant)),
